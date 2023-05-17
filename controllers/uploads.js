3d0c576d5b0629611856960fba4efb3fbb51698f
gabriel <gabrielmancilladucuara@gmail.com>
2023-05-17 09:20:41 -0500

    subiendo archivos al uploads

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,16 +1,27 @@
-const { response } = require('express')
+const path = require('path')
 
+const { response } = require("express");
 
-const cargarArchivo = (req , res = response) => {
+const cargarArchivo = (req, res = response) => {
+  
+  if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+    res.status(400).json({msg:'No hay archivos que subir'});
+    return;
+  }
 
-    res.json({
-        msg:'Hola mundo'
-    })
+  const {archivo} = req.files;
 
+  const uploadPath = path.join( __dirname, "../uploads/", archivo.name );
 
-}
+  archivo.mv(uploadPath, (err) => {
+    if (err) {
+      return res.status(500).json({err});
+    }
+
+    res.json({msg: 'File upload to' + uploadPath});
+  });
+};
 
 module.exports = {
-    cargarArchivo
-}
-
+  cargarArchivo,
+};
--- models/server.js
+++ models/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { dbConnection } = require("../database/config.js");
+const fileUpload = require("express-fileupload");
 
 class Server {
   constructor() {
@@ -35,6 +36,12 @@ class Server {
     this.app.use(express.json())
     //Directorio publicos
     this.app.use(express.static("public"));
+
+    //Fileupload - Carga de archivos
+    this.app.use(fileUpload({
+      useTempFiles: true,
+      tempFileDir: '/tmp/'
+    }))
   }
 
   routes() {
